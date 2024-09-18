717e584bd849d51d5d262efdb001d3bec9ae7806
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-09-18 17:56:49 +0300

    Add project files.

---
+++ UdemyMvcPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyMvcPortfolio.Models.Entity;
+
+
+namespace UdemyMvcPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCVEntities db=new DbCVEntities();
+        public ActionResult Index()
+        {
+            var values = db.About.ToList();
+            return View(values);
+        }
+
+        public PartialViewResult Experience()
+        {
+            var values=db.Experience.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Education()
+        {
+            var values = db.Education.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Skills()
+        {
+            var values = db.Skills.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Hobbies()
+        {
+            var values = db.Hobbies.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Certificates()
+        {
+            var values = db.Certificates.ToList();
+            return PartialView(values);
+        }
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Contact(Contact contact)
+        {
+            contact.Date =DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Contact.Add(contact);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+    }
+}
---
+++ UdemyMvcPortfolio/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UdemyMvcPortfolio.Models.Entity;
+
+namespace UdemyMvcPortfolio.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCVEntities db = new DbCVEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+        public void TAdd(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+        public void TUpdate(T entity)
+        {
+            db.SaveChanges();
+        }
+        public void TDelete(T entity)
+        {
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+    }
+}
